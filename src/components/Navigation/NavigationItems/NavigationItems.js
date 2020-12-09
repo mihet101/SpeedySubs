f1dbee296bd0025fde12b539f57aa3233191bdc9
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 14:45:14 -0500

    Update NavigationItems.js

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,6 +3,17 @@ import React from 'react';
 import classes from './NavigationItems.css'
 import NavigationItem from './NavigationItem'
 
+/*Function Name= navigatioItems
+Description= Helps to navigate through pages
+Coder Name= Kush
+Approved= Saurav
+Start Date= Oct 1st, 2020
+End Date= Oct 1st, 2020
+Code Approved= Oct 1st
+Input= props(isAuthed) 
+Output= navigation buttons
+*/
+
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem exact link="/">Burger Builder</NavigationItem>
