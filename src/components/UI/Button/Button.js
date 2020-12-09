0bb5c6985687c86e90836dc9a3c215869cd408bb
sks-99 <72107325+sks-99@users.noreply.github.com>
2020-12-09 15:03:14 -0500

    Update Button.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import classes from './Button.css'
-
+/*Name= Button
+*Coder= Tim
+*Approved= Sourav
+*Start= 2nd
+*End= oct 2nd
+*Variable= button
+*/
 const button = (props) => (
     <button 
         disabled={props.disabled}
