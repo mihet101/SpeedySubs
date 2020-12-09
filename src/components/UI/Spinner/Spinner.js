3f7a9045f0cc6d8ad4289af1aa3a40e3e424fd53
sks-99 <72107325+sks-99@users.noreply.github.com>
2020-12-09 15:42:24 -0500

    Update Spinner.js

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import classes from './Spinner.css';
-
+/*Name= Spinner
+* Description: Page Loading
+*Coder= Sumeet
+*Approved= Tim
+*Start= Oct 12
+*End= Oct 12
+*Variable= spinner
+*/
 const spinner = () => (
     <div className={classes.Loader}>Loading...</div>
 );
