3bc8815f2c85bed61050b0db123fb233347f3a8b
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 14:56:39 -0500

    Update Logout.js

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -4,6 +4,15 @@ import { connect } from 'react-redux';
 
 import * as actions from '../../../store/actions/index';
 
+/*Function Name= Logout
+Description= Helps logout
+Coder Name= Sumeet
+Approved= Tim
+Start Date= Oct 5th, 2020
+End Date= Oct 5th, 2020
+Code Approved= Oct 5th
+*/
+
 class Logout extends Component {
     componentDidMount () {
         this.props.onLogout();
