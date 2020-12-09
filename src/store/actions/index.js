042bdf8f78ea467565a66c9971c1319ae1b0c3d9
SPandya23 <72760142+SPandya23@users.noreply.github.com>
2020-12-09 15:23:44 -0500

    Update index.js

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,12 @@
+/*
+Description= initializes the ingredient buttons, orders, logout, etc
+Coder Name= Sourav
+Start Date= Nov 21st, 2020
+End Date= Nov 21st, 2020
+Code Approved= Tim
+
+*/
+
 export {
     addIngredient,
     removeIngredient,
