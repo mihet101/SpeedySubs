a59c7b307b9624b8710149b201effb70a525130a
sks-99 <72107325+sks-99@users.noreply.github.com>
2020-12-09 14:30:38 -0500

    Update BuildControl.js

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import classes from './BuildControl.css'
-
+/*Name= BuildControl
+* Description: Add & Subtract Ingredients
+*Coder= Sumeet
+*Approved= Kush
+*Start= Sept 30
+*End= Sept 30
+*Variable= buildControl
+*/
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
