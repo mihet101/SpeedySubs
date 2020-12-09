427c8e963948e1a30e94c13a3919a822dc4f75cc
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 14:52:30 -0500

    Update Toolbar.js

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,6 +4,18 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
+/*Function Name= toolbar
+Description= upper bar to show logo and menu items
+Coder Name= Tim
+Approved= Sumeet
+Start Date= Oct 3rd, 2020
+End Date= Oct 3rd, 2020
+Code Approved= Oct 3rd
+Arguments= Toolbar.css
+Input= Props(drawertoggleClicked) 
+Output= upper menu bar
+*/
+
 const toolbar = (props)=>(
     <header className={classes.Toolbar}>
         <DrawerToggle clicked={props.drawerToggleClicked}/>
