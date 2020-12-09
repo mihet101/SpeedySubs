3f56ec52adb0f55cb9c50e5db36d7e9df79fb79d
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 14:47:24 -0500

    Update SideDrawer.js

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,6 +5,18 @@ import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/backdrop'
 import Aux from '../../../hoc/Auxilliary/Auxilliary'
 
+/*Function Name= SideDrawer
+Description= Side menu for mobile to navigate
+Coder Name= Kush
+Approved= Tim
+Start Date= Oct 2nd, 2020
+End Date= Oct 2nd, 2020
+Code Approved= Oct 2nd
+Arguments= SideDrawer.css
+Input= props(open) 
+Output= Side menu for mobile devices
+*/
+
 const sideDrawer = (props)=>{
     let attachedClasses = [classes.SideDrawer,classes.Close];
     if(props.open){
