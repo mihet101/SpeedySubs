8476895fab48679c01074ae5716064c6d4f1716f
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 14:42:12 -0500

    Update NavigationItem.js

--- src/components/Navigation/NavigationItems/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem.js
@@ -2,6 +2,17 @@ import React from 'react';
 import classes from './NavigationItem.css';
 import {NavLink} from 'react-router-dom';
 
+/*Function Name= navigationItem
+Description= navigation buttons
+Coder Name= Summet
+Approved = Tim
+Start Date= Oct 1st, 2020
+End Date= Oct 1st, 2020
+Code Approved= Oct 1st
+Input= props(links)
+Output= navigation buttons
+*/
+
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
         <NavLink
