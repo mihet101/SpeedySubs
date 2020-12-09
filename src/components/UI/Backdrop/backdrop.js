92e43e7cabab702d5d0c68264df732edc562693b
sks-99 <72107325+sks-99@users.noreply.github.com>
2020-12-09 14:53:02 -0500

    Update backdrop.js

--- src/components/UI/Backdrop/backdrop.js
+++ src/components/UI/Backdrop/backdrop.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import classes from './backdrop.css'
-
+/*Name= Backdrop
+*Description= Removes the pop up when the user clicks outside of the box   
+*Coder= Sumeet
+*Approved= Kush
+*Start= Sept 30 2020
+*End= Sept 30 2020
+*Variable= backdrop
+*/
 const backdrop = (props) => (
     props.show?<div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
