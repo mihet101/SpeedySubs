f5522da3b1d1c23b7fccfdd10cb5f4a8fe00eac0
sks-99 <72107325+sks-99@users.noreply.github.com>
2020-12-09 15:39:59 -0500

    Update Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,8 +3,16 @@ import classes from './Modal.css';
 import Aux from '../../../hoc/Auxilliary/Auxilliary';
 import Backdrop from '../Backdrop/backdrop'
 
+/*Name= BuildControls
+* Description: Pop up creation
+*Coder= Sourav
+*Approved= Sumeet
+*Start= Oct 10
+*End= Oct 10
+*Variable= 
+*/
 class Modal extends Component {
-    shouldComponentUpdate(nextProps,nextState){
+    shouldComponentUpdate(nextProps , nextState){
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
