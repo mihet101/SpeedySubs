b92baa8becde14907088443a10ae60e3f92a61cc
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2020-12-09 15:04:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
+/*Description= Initiated the application
+Coder Name= Kush, Tim
+Approved= Saurav, Sumeet
+Start Date= Sept 28th, 2020
+End Date= Sept 28th, 2020
+Code Approved= Sept 28th
+Arguments= index.css
+File Accessed= all files used by the app
+*/
+
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
